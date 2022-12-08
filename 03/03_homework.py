032eaefd4b80935d5142c20b23503e6d0cc0dc55
Oleh Petraniuk <petroleg1302@gmail.com>
2022-12-08 11:09:03 +0100

    freshly done homework

---
+++ 03/03_homework.py
@@ -0,0 +1,16 @@
+print("I love Python"*42)
+age_in_months = 24 * 12 + 10
+age_in_years = age_in_months//12
+my_name = "Oleh"
+my_age = "My name is " + my_name + " I'm " + str(age_in_years) + " years old"
+also_my_age = f"My name is {my_name} I'm {age_in_years} years old"
+a_variable = 1
+print(a_variable == 1)
+print(a_variable != 1)
+print(a_variable > 2)
+print(a_variable <= 0)
+print(-1<a_variable<5)
+a = 2
+b = 5
+c = 6
+d = str(a) + str(b) + str(c)
