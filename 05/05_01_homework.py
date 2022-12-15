1dca929fd641b2ea7be068467c365cd8fff29ded
Oleh Petraniuk <petroleg1302@gmail.com>
2022-12-15 20:56:12 +0100

    Found some mistakes before pull request

--- 05/05_01_homework.py
+++ 05/05_01_homework.py
@@ -1,14 +1,14 @@
 user_input = input("Enter something: ")
 for element in user_input:
- if element.isdigit():
-     if int(element) % 2 == 0:
-         print(f'The number "{element}" is even.')
-     else:
-         print(f'The number "{element}" is odd.')
- elif element.isalpha():
-     if element.isupper():
-         print(f'The letter "{element}" is capital.')
-     else:
-         print(f'The letter "{element}" isn\'t capital.')
- else:
-     print(f'"{element}" is a symbol')
+    if element.isdigit():
+        if int(element) % 2 == 0:
+            print(f'The number "{element}" is even.')
+        else:
+            print(f'The number "{element}" is odd.')
+    elif element.isalpha():
+        if element.isupper():
+            print(f'The letter "{element}" is capital.')
+        else:
+            print(f'The letter "{element}" isn\'t capital.')
+    else:
+        print(f'"{element}" is a symbol')
--- 05/05_02_homework.py
+++ 05/05_02_homework.py
@@ -1,4 +1,5 @@
 import time
 
- while True:
-     print("I love python")
+while True:
+    print("I love python")
+    time.sleep(4.2)
